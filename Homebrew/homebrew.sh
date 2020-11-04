c7defdb7763f19e0c25e353a241074e669fec478
Jonas Sander <jonas.sander@t-online.de>
2020-11-04 21:35:31 +0100

    add more apps

--- Homebrew/homebrew.sh
+++ Homebrew/homebrew.sh
@@ -14,6 +14,12 @@ brew install git
 # improved top (interactive process viewer)
 brew install htop
 
+# fuse for macos allows you to extend macos's native file handling capabilities via third-party file systems (needed for sshfs)
+brew cask install osxfuse
+
+# file system client based on ssh file transfer protocol
+brew install sshfs
+
 # open-source version of the X.Org X window system
 brew cask install xquartz
 
@@ -25,6 +31,9 @@ brew install python3
 
 # iTerm2 is a terminal emulator for macOS that does amazing things
 brew cask install iterm2
+
+# open the termina from everywhere in the finder
+brew cask install go2shell
 
 # windows alt-tab on macOS (i.e. switch between windows, not apps)
 brew cask install alt-tab
@@ -65,6 +74,12 @@ brew cask install tikzit
 # text editor for code, markup and prose
 brew cask install sublime-text
 
+# vs as editor
+brew cask install visual-studio-code
+
+# run container
+brew cask install docker
+
 # free and open-source ide for python programming - community edition
 brew cask install pycharm-ce
 
@@ -73,6 +88,18 @@ brew cask install enpass
 
 # the world of music
 brew cask install spotify
+
+# powerful note-takin app
+brew cask install evernote
+
+# a vpn client
+brew cask install tunnelblick
+
+# app uninstaller
+brew cask install appcleaner
+
+# a heay internet browser that eats all your ram
+brew cask install google-chrome
 
 # remove outdated stuff
 brew cleanup
