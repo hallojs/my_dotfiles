a106e94430da42de4d4c599887f3ae034e87df61
Jonas Sander <jonas.sander@t-online.de>
2020-12-27 11:18:35 +0100

    Update cask syntax, add zoom, fix iina install

--- Homebrew/homebrew.sh
+++ Homebrew/homebrew.sh
@@ -15,7 +15,7 @@ brew install git
 brew install htop
 
 # fuse for macos allows you to extend macos's native file handling capabilities via third-party file systems (needed for sshfs)
-brew cask install osxfuse
+brew install --cask osxfuse
 
 # file system client based on ssh file transfer protocol
 brew install sshfs
@@ -24,7 +24,7 @@ brew install sshfs
 brew install wget
 
 # open-source version of the X.Org X window system
-brew cask install xquartz
+brew install --cask xquartz
 
 # visually compare two PDF files
 brew install diff-pdf
@@ -33,109 +33,112 @@ brew install diff-pdf
 brew install python3
 
 # iTerm2 is a terminal emulator for macOS that does amazing things
-brew cask install iterm2
+brew install --cask iterm2
 
 # open the termina from everywhere in the finder
-brew cask install go2shell
+brew install --cask go2shell
 
 # windows alt-tab on macOS (i.e. switch between windows, not apps)
-brew cask install alt-tab
+brew install --cask alt-tab
 
 # a window management app based on Spectacle, written in Swift
-brew cask install rectangle
+brew install --cask rectangle
 
 # a ultra-light macOS utility that helps hide menu bar icons
-brew cask install hiddenbar
+brew install --cask hiddenbar
 
 # a simple macOS system monitor in your menu bar
-brew cask install stats
+brew install --cask stats
 
 # a dimness and night shift menu bar app
-brew cask install joshjon-nocturnal
+brew install --cask joshjon-nocturnal
 
 # etcher is a powerful OS image flasher
-brew cask install balenaetcher
+brew install --cask balenaetcher
 
 # oracle jdk
-brew cask install oracle-jdk
+brew install --cask oracle-jdk
 
 # whatsapp desktop client
-brew cask install whatsapp
+brew install --cask whatsapp
 
 # slack desktop client
-brew cask install slack
+brew install --cask slack
 
 # full tex live distribution with gui applications
-brew cask install mactex
+brew install --cask mactex
 
 # my favourite latex editor
-brew cask install texpad
+brew install --cask texpad
 
 # my backup latex editor
-brew cask install texstudio
+brew install --cask texstudio
 
 # pgf/TikZ diagram editor
-brew cask install tikzit
+brew install --cask tikzit
 
 # text editor for code, markup and prose
-brew cask install sublime-text
+brew install --cask sublime-text
 
 # vs as editor
-brew cask install visual-studio-code
+brew install --cask visual-studio-code
 
 # run container
-brew cask install docker
+brew install --cask docker
 
 # free and open-source ide for python programming - community edition
-brew cask install pycharm-ce
+brew install --cask pycharm-ce
 
 # password and credentials mananger
-brew cask install enpass
+brew install --cask enpass
 
 # the world of music
-brew cask install spotify
+brew install --cask spotify
 
 # powerful note-takin app
-brew cask install evernote
+brew install --cask evernote
 
 # a vpn client
-brew cask install tunnelblick
+brew install --cask tunnelblick
 
 # app uninstaller
-brew cask install appcleaner
+brew install --cask appcleaner
 
 # a heay internet browser that eats all your ram
-brew cask install google-chrome
+brew install --cask google-chrome
 
 # cross-platform web browser with a free vpn
-brew cask install opera
+brew install --cask opera
 
 # web browser focusing on security
-brew cask install tor-browser
+brew install --cask tor-browser
 
 # sync extension for google drive
-brew cask install google-backup-and-sync
+brew install --cask google-backup-and-sync
 
 # multi-language programming editor
-brew cask install coderunner
+brew install --cask coderunner
 
 # utility to provide quick look previews for files that aren't natively supported
-brew cask install glance
+brew install --cask glance
 
 # open-source cross-platform multimedia player
-brew cask install vlc
+brew install --cask vlc
 
 # video and audio player
-brew cask install elmedia-player
+brew install --cask elmedia-player
 
 # video chat, voice call and instant messaging application
-brew cask install skype
+brew install --cask skype
+
+# Video communication and virtual meeting platform
+brew install --cask zoom
 
 # remote access and connectivity software focused on security
-brew cask install teamviewer
+brew install --cask teamviewer
 
 # free and open-source media player
-brew cask install 
+brew install --cask iina
 
 # remove outdated stuff
 brew cleanup
